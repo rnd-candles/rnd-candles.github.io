56a03b19bb1c16b4d23adefaea6470d399bb2c6e
Fedichkin Denis <fedichkin.dy@gmail.com>
2021-07-21 16:43:33 +0300

    add cylinder with flowers and st nikolay

---
+++ js/catalog/ru.js
@@ -0,0 +1,140 @@
+(function() {
+    if (!window.catalog) {
+        window.catalog = {};
+    }
+
+    window.catalog.ru = {
+        figureCandles: [
+            {
+                name     : "Тыква",
+                image    : "pumpkin_orange.jpg",
+                size     : "Диаметр 3,5 см",
+                weight   : "Вес 23 грамм",
+                article  : "Артикул: 1001",
+                addPrice : "С ароматом 15 гривен",
+                basePrice: "15 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Подарок",
+                image    : "gift_box_blue.jpg",
+                size     : "Длина, ширина и высота 3,5 см",
+                weight   : "Вес 35 грамм",
+                article  : "Артикул: 1002",
+                addPrice : "С ароматом 25 гривен",
+                basePrice: "25 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Сота",
+                image    : "cell_gray.jpg",
+                size     : "Диаметр 6 см и высота 4 см",
+                weight   : "Вес 80 грамм",
+                article  : "Артикул: 1003",
+                addPrice : "С ароматом 40 гривен",
+                basePrice: "35 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Листья",
+                image    : "leaves_red.jpg",
+                size     : "Диаметр 4,5 см и высота 7 см",
+                weight   : "Вес 100 грамм",
+                article  : "Артикул: 1004",
+                addPrice : "С ароматом 50 гривен",
+                basePrice: "45 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Шишка",
+                image    : "cone_brown.jpg",
+                size     : "Диаметр 7 см и высота 8 см",
+                weight   : "Вес 100 грамм",
+                article  : "Артикул: 1005",
+                addPrice : "С ароматом 45 гривен",
+                basePrice: "40 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Лотус",
+                image    : "lotus_pink.jpg",
+                size     : "Диаметр 7 см и высота 5 см",
+                weight   : "Вес 75 грамм",
+                article  : "Артикул: 1006",
+                addPrice : "С ароматом 50 гривен",
+                basePrice: "45 гривен",
+                candlewick: 3
+            },
+            {
+                name     : "Зефирка",
+                image    : "marshmallow_light_purple.jpg",
+                size     : "Диаметр 8 см и высота 8 см",
+                weight   : "Вес 110 грамм",
+                article  : "Артикул: 1007",
+                addPrice : "С ароматом 55 гривен",
+                basePrice: "50 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Узел",
+                image    : "node_pink.jpg",
+                size     : "Диаметр 8 см и высота 7 см",
+                weight   : "Вес 110 грамм",
+                article  : "Артикул: 1008",
+                addPrice : "С ароматом 55 гривен",
+                basePrice: "50 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Вязаная свеча",
+                image    : "knitting_orange.jpg",
+                size     : "Диаметр 5 см и высота 7 см",
+                weight   : "Вес 140 грамм",
+                article  : "Артикул: 1009",
+                addPrice : "С ароматом 65 гривен",
+                basePrice: "60 гривен",
+                candlewick: 3
+            },
+            {
+                name     : "Женский торс",
+                image    : "woman_body-light_yellow.jpg",
+                size     : "Высота 10 см",
+                weight   : "Вес 85 грамм",
+                article  : "Артикул: 1010",
+                addPrice : "С ароматом 65 гривен",
+                basePrice: "60 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Мужской торс",
+                image    : "man_body-light_yellow.jpg",
+                size     : "Высота 10 см",
+                weight   : "Вес 85 грамм",
+                article  : "Артикул: 1011",
+                addPrice : "С ароматом 65 гривен",
+                basePrice: "60 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Цилиндр с цветком",
+                image    : "cylinder_flowers-beige.jpg",
+                size     : "Диаметр 3,5 см и высота 5,5 см",
+                weight   : "Вес 60 грамм",
+                article  : "Артикул: 1012",
+                addPrice : "С ароматом 35 гривен",
+                basePrice: "30 гривен",
+                candlewick: 2
+            },
+            {
+                name     : "Святой Николай",
+                image    : "st_nikolay-beige.jpg",
+                size     : "Диаметр 3,5 см и высота 5,5 см",
+                weight   : "Вес 55 грамм",
+                article  : "Артикул: 1013",
+                addPrice : "С ароматом 35 гривен",
+                basePrice: "30 гривен",
+                candlewick: 2
+            }
+        ]
+    };
+})();
---
+++ js/catalog/ua.js
@@ -0,0 +1,140 @@
+(function() {
+    if (!window.catalog) {
+        window.catalog = {};
+    }
+
+    window.catalog.ua = {
+        figureCandles: [
+            {
+                name     : "Гарбузик",
+                image    : "pumpkin_orange.jpg",
+                size     : "Діаметр 3,5 см",
+                weight   : "Вага 23 грам",
+                article  : "Артикул: 1001",
+                addPrice : "З ароматом 15 гривень",
+                basePrice: "15 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "Подарунок",
+                image    : "gift_box_blue.jpg",
+                size     : "Довжина, ширина і висота 3,5 см",
+                weight   : "Вага 35 грам",
+                article  : "Артикул: 1002",
+                addPrice : "З ароматом 25 гривень",
+                basePrice: "25 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "Сота",
+                image    : "cell_gray.jpg",
+                size     : "Діаметр 6 см і висота 4 см",
+                weight   : "Вага 80 грам",
+                article  : "Артикул: 1003",
+                addPrice : "З ароматом 40 гривень",
+                basePrice: "35 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "Листя",
+                image    : "leaves_red.jpg",
+                size     : "Діаметр 4,5 см і висота 7 см",
+                weight   : "Вага 100 грам",
+                article  : "Артикул: 1004",
+                addPrice : "З ароматом 50 гривень",
+                basePrice: "45 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "Шишка",
+                image    : "cone_brown.jpg",
+                size     : "Діаметр 7 см і висота 8 см",
+                weight   : "Вага 100 грам",
+                article  : "Артикул: 1005",
+                addPrice : "З ароматом 45 гривень",
+                basePrice: "40 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "Лотус",
+                image    : "lotus_pink.jpg",
+                size     : "Діаметр 7 см і висота 5 см",
+                weight   : "Вага 75 грам",
+                article  : "Артикул: 1006",
+                addPrice : "З ароматом 50 гривень",
+                basePrice: "45 гривень",
+                candlewick: 3
+            },
+            {
+                name     : "Зефірка",
+                image    : "marshmallow_light_purple.jpg",
+                size     : "Діаметр 8 см і висота 8 см",
+                weight   : "Вага 110 грам",
+                article  : "Артикул: 1007",
+                addPrice : "З ароматом 55 гривень",
+                basePrice: "50 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "Вузол",
+                image    : "node_pink.jpg",
+                size     : "Діаметр 8 см і висота 7 см",
+                weight   : "Вага 110 грам",
+                article  : "Артикул: 1008",
+                addPrice : "З ароматом 55 гривень",
+                basePrice: "50 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "В'язана свічка",
+                image    : "knitting_orange.jpg",
+                size     : "Діаметр 5 см і висота 7 см",
+                weight   : "Вага 140 грам",
+                article  : "Артикул: 1009",
+                addPrice : "З ароматом 65 гривень",
+                basePrice: "60 гривень",
+                candlewick: 3
+            },
+            {
+                name     : "Жіночий торс",
+                image    : "woman_body-light_yellow.jpg",
+                size     : "Висота 10 см",
+                weight   : "Вага 85 грам",
+                article  : "Артикул: 1010",
+                addPrice : "З ароматом 65 гривень",
+                basePrice: "60 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "Чоловічий торс",
+                image    : "man_body-light_yellow.jpg",
+                size     : "Висота 10 см",
+                weight   : "Вага 95 грам",
+                article  : "Артикул: 1011",
+                addPrice : "З ароматом 65 гривень",
+                basePrice: "60 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "Циліндр з квіткою",
+                image    : "cylinder_flowers-beige.jpg",
+                size     : "Діаметр 3,5 см і висота 5,5 см",
+                weight   : "Вага 60 грам",
+                article  : "Артикул: 1012",
+                addPrice : "З ароматом 35 гривень",
+                basePrice: "30 гривень",
+                candlewick: 2
+            },
+            {
+                name     : "Святий Миколай",
+                image    : "st_nikolay-beige.jpg",
+                size     : "Діаметр 3,5 см і висота 5,5 см",
+                weight   : "Вага 55 грам",
+                article  : "Артикул: 1013",
+                addPrice : "З ароматом 35 гривень",
+                basePrice: "30 гривень",
+                candlewick: 2
+            }
+        ]
+    };
+})();
